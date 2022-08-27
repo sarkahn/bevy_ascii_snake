15769fa1434345ba33ad72a947e30a2152a38382
sark <sarkahn@hotmail.com>
2022-08-27 02:05:27 -0600

    Start screen, end screen, sounds

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,52 @@ use std::collections::VecDeque;
 
 use bevy::prelude::{*};
 use bevy::DefaultPlugins;
-use bevy_ascii_terminal::{prelude::*, code_page_437, ToWorld};
+use bevy_ascii_terminal::{prelude::*};
+use bevy_kira_audio::{AudioPlugin, Audio, AudioControl, AudioSource};
 use rand::Rng;
 use rand::rngs::ThreadRng;
 use window::WindowPlugin;
 
 const STAGE_SIZE: IVec2 = IVec2::from_array([40,40]);
+const START_SPEED: f32 = 8.0;
 const ACCELERATION: f32 = 0.35;
 const MAX_SPEED: f32 = 35.;
 const BODY_GLYPH: char = '█';
 const FOOD_GLYPH: char = '☼';
 
+#[derive(Debug, StageLabel, Clone, Eq, PartialEq, Hash)]
+enum GameState {
+    Begin,
+    Playing,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(TerminalPlugin)
+        .add_plugin(AudioPlugin)
         .add_plugin(WindowPlugin)
+        .init_resource::<FoodCount>()
+        .init_resource::<Sounds>()
+        .add_state(GameState::Begin)
         .add_startup_system(setup)
-        .add_system(make_food)
-        .add_system(drive.after(make_food))
-        .add_system(eat.after(drive))
-        .add_system(grow.after(eat))
-        .add_system(render.after(grow))
-        .add_system(die.after(render))
+        .add_system_set(
+            SystemSet::on_update(GameState::Begin)
+                .with_system(start)
+        )
+        .add_system_set(
+            SystemSet::on_enter(GameState::Playing)
+                .with_system(spawn)
+        )
+        .add_system_set(
+            SystemSet::on_update(GameState::Playing)
+                .with_system(make_food)
+                .with_system(drive.after(make_food))
+                .with_system(eat.after(drive))
+                .with_system(grow.after(eat))
+                .with_system(render.after(grow))
+                .with_system(die.after(render))
+        )
         .run();
 }
 
@@ -58,25 +81,63 @@ struct Grow {
     pos: IVec2,
 }
 
+#[derive(Default)]
+struct FoodCount(usize);
+
+#[derive(Default)]
+struct Sounds {
+    nom: Handle<AudioSource>,
+    ouch: Handle<AudioSource>,
+    ding: Handle<AudioSource>,
+}
+
+
 fn setup(
     mut commands: Commands,
+    server: Res<AssetServer>,
+    mut sfx: ResMut<Sounds>,
 ) {
     let mut term = Terminal::with_size([42,42]);
     term.draw_border(BorderGlyphs::single_line());
+    term.draw_box([0,5].pivot(Pivot::Center), [13,3], UiBox::double_line().color_fill(Color::GRAY, Color::BLACK));
+    term.put_string([-5,5].pivot(Pivot::Center), "ASCII SNAKE".fg(Color::BLUE));
+    term.put_string([-6,2].pivot(Pivot::Center), "Use WASD to move");
+    term.put_string([-9,1].pivot(Pivot::Center), "Press Space to Begin");
 
     commands.spawn_bundle(TerminalBundle::from(term))
-      .insert(ToWorld::default())
       .insert(AutoCamera);
+    
+    sfx.nom = server.load("nom.wav");
+    sfx.ouch = server.load("ouch.wav");
+    sfx.ding = server.load("ding.wav");
+}
 
+fn start(
+    input: Res<Input<KeyCode>>,
+    mut state: ResMut<State<GameState>>,
+    audio: Res<Audio>,
+    sfx: Res<Sounds>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        state.set(GameState::Playing).unwrap();
+        audio.play(sfx.ding.clone());
+    }
+}
+
+fn spawn(
+    mut commands: Commands,
+    mut count: ResMut<FoodCount>,
+) {
     let body = Body(VecDeque::from(vec![IVec2::ZERO]));
     let steering = Steering {
         cell_pos: 0.5,
         dir: [0,1].into(),
-        speed: 5.0,
+        speed: START_SPEED,
         prev: IVec2::ZERO,
     };
     let grid_pos = GridPos([0,0].into());
     commands.spawn().insert(body).insert(steering).insert(grid_pos);
+    count.0 = 0;
 }
 
 fn drive(
@@ -136,8 +197,9 @@ fn make_food(
         if let Ok(body) = q_body.get_single() {
             let body = &body.0;
             loop {
-                let pos = rand_pos(&mut rng, STAGE_SIZE);
-                let pos = pos - STAGE_SIZE / 2;
+                let x = rng.gen_range(0..STAGE_SIZE.x);
+                let y = rng.gen_range(0..STAGE_SIZE.y);
+                let pos = IVec2::new(x,y) - STAGE_SIZE / 2;
 
                 if body.contains(&pos) || !in_bounds(pos) {
                     continue;
@@ -152,25 +214,18 @@ fn make_food(
     }
 }
 
-fn rand_pos(rng: &mut ThreadRng, dimensions: IVec2) -> IVec2 {
-    let x = rng.gen_range(0..dimensions.x);
-    let y = rng.gen_range(0..dimensions.y);
-    IVec2::new(x,y)
-}
-
 fn render(
-    mut q_term: Query<(&mut Terminal, &ToWorld)>,
+    mut q_term: Query<&mut Terminal>,
     q_snake: Query<&Body, Changed<Body>>,
     q_food: Query<&Food>,
 ) {
     if let Ok(body) = q_snake.get_single() {
         let body = &body.0;
-        let (mut term, tw) = q_term.single_mut();
+        let mut term = q_term.single_mut();
 
         term.clear();
         term.draw_border(BorderGlyphs::single_line());
         for food in &q_food {
-            // Add one to account for borders
             let pos = food.pos + STAGE_SIZE / 2;
             term.put_char(pos, FOOD_GLYPH);
         }
@@ -185,17 +240,21 @@ fn eat(
     q_food: Query<(Entity, &Food)>,
     mut q_snake: Query<(&Body, &mut Steering, &GridPos), Changed<GridPos>>,
     mut commands: Commands,
+    mut count: ResMut<FoodCount>,
+    audio: Res<Audio>,
+    sfx: Res<Sounds>,
 ) {
     for (body, mut steering, pos) in &mut q_snake {
         for (e_food, food) in &q_food {
-            //println!("Snake pos {}, food pos {}", pos.0, food.pos);
             if pos.0 == food.pos {
+                count.0 += 1;
                 commands.entity(e_food).despawn();
                 steering.speed = (steering.speed + ACCELERATION).min(MAX_SPEED);
                 commands.spawn().insert(Grow {
-                    turns: body.0.len(),
-                    pos: pos.0,
+                    turns: count.0,
+                    pos: *body.0.back().unwrap(),
                 });
+                audio.play(sfx.nom.clone());
             }
         } 
     }
@@ -205,25 +264,25 @@ fn grow(
     mut q_grow: Query<(Entity, &mut Grow)>,
     mut q_snake: Query<(&mut Body, &GridPos), Changed<GridPos>>, 
     mut commands: Commands,
+    count: Res<FoodCount>,
 ) {
     if q_snake.is_empty() {
         return;
     }
 
-    for _ in &q_snake {
-        for (_, mut grow) in &mut q_grow {
-            grow.turns -= 1;
+    let mut body = q_snake.single_mut().0;
+    for (entity, mut grow) in &mut q_grow {
+        if grow.turns <= count.0 {
+            body.0.push_back(grow.pos);
+        }
+
+        grow.turns -= 1;
+
+        if grow.turns == 0 {
+            commands.entity(entity).despawn();
         }
     }
 
-    let mut body = q_snake.single_mut().0;
-    for (entity, grow) in &q_grow {
-        if grow.turns != 0 {
-            continue;
-        }
-        body.0.push_back(grow.pos);
-        commands.entity(entity).despawn();
-    }
 }
 
 fn die(
@@ -231,13 +290,18 @@ fn die(
     q_food: Query<Entity, With<Food>>,
     mut q_term: Query<&mut Terminal>,
     mut commands: Commands,
+    mut state: ResMut<State<GameState>>,
+    audio: Res<Audio>,
+    sfx: Res<Sounds>,
 ) {
     let mut game_over = |entity| {
         commands.entity(entity).despawn();
         q_food.for_each(|e|commands.entity(e).despawn());
         let mut term = q_term.single_mut();
         term.clear();
-        term.put_string([-5,1].pivot(Pivot::Center), "Game Over!");
+        term.put_string([-15,1].pivot(Pivot::Center), "Game Over! Press Space to restart");
+        state.set(GameState::Begin).unwrap();
+        audio.play(sfx.ouch.clone());
     };
 
     if let Ok((snake_entity, pos, body)) = q_snake.get_single() {
@@ -256,5 +320,5 @@ fn die(
 fn in_bounds(p: IVec2) -> bool {
     let half_stage = STAGE_SIZE / 2;
 
-    !(p.cmple(-half_stage).any() || p.cmpge(half_stage).any())
+    !(p.cmple(-half_stage).any() || p.cmpge(half_stage + 1).any())
 }
--- src/window.rs
+++ src/window.rs
@@ -25,7 +25,7 @@ impl Plugin for WindowPlugin {
 // Sets the icon on windows and X11
 fn set_window_icon(windows: NonSend<WinitWindows>) {
     let primary = windows.get_window(WindowId::primary()).unwrap();
-    let icon_buf = Cursor::new(include_bytes!("../assets/textures/bevy.png"));
+    let icon_buf = Cursor::new(include_bytes!("../assets/icon.png"));
     if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
         let image = image.into_rgba8();
         let (width, height) = image.dimensions();
